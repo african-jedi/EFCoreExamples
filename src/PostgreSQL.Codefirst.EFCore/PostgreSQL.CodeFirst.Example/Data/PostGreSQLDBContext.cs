a340a5cb21792265703f2f81913159e1b3f126d0
African Jedi <fk.posh697@passinbox.com>
2025-12-29 18:40:16 +0200

    Update Program file to  load at least 2 products

--- src/PostgreSQL.Codefirst.EFCore/PostgreSQL.CodeFirst.Example/Data/PostGreSQLDBContext.cs
+++ src/PostgreSQL.Codefirst.EFCore/PostgreSQL.CodeFirst.Example/Data/PostGreSQLDBContext.cs
@@ -1,9 +1,12 @@
+using EFCore.CodeFirst.Models;
 using Microsoft.EntityFrameworkCore;
 
 namespace PostgreSQL.CodeFirst.Example.Data;
 
 public class PostGreSQLDBContext : DbContext
 {
+    public DbSet<Product> Products { get; set; }
+    public DbSet<Category> Categories { get; set; }
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
         if (!optionsBuilder.IsConfigured)
--- src/PostgreSQL.Codefirst.EFCore/PostgreSQL.CodeFirst.Example/Program.cs
+++ src/PostgreSQL.Codefirst.EFCore/PostgreSQL.CodeFirst.Example/Program.cs
@@ -1,2 +1,42 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using EFCore.CodeFirst.Models;
+using Microsoft.EntityFrameworkCore;
+using PostgreSQL.CodeFirst.Example.Data;
+
 Console.WriteLine("Hello, World!");
+using PostGreSQLDBContext context = new();
+
+Category category = new()
+{
+    CategoryName = "Electronics",
+};
+
+Category category2 = new()
+{
+    CategoryName = "Food",
+};
+
+Product product1 = new()
+{
+    Category = category,
+    Name = "Iphone 17",
+    Price = 20000
+};
+
+Product product2 = new()
+{
+    Category = category,
+    Name = "Ipad 10",
+    Price = 20000
+};
+
+await context.Products.AddAsync(product1);
+await context.Products.AddAsync(product2);
+context.SaveChanges();
+
+var products = context.Products.Include(c => c.Category)
+                       .AsNoTracking()
+                       .ToList();
+
+foreach (var item in products)
+    Console.WriteLine($"{item.RowId}: {item.Name} - {item.Category.CategoryName}");
